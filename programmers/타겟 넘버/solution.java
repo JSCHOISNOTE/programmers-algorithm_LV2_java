251d9cfe51985e3d4e35576c62e322ffea09ae24
JEONG SEOK CHOI <109507834+JSCHOISNOTE@users.noreply.github.com>
2023-01-09 11:14:56 +0900

    uploaded at 2023. 1. 9. 오전 11:15:45

---
+++ programmers/타겟 넘버/solution.java
@@ -0,0 +1,23 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/43165
+
+class Solution {
+    int answer = 0;
+
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, 0, target, 0);
+
+        return answer;
+    }
+
+    // 깊이 우선 탐색
+    public void dfs(int[] numbers, int depth, int target, int sum){
+        if(depth == numbers.length){ // 마지막 노드 까지 탐색한 경우
+            if(target == sum) answer++;
+        } else {
+            // 해당 노드의 값을 더하고 다음 깊이 탐색
+            dfs(numbers, depth + 1, target, sum + numbers[depth]); 
+            // 해당 노드의 값을 빼고 다음 깊이 탐색
+            dfs(numbers, depth + 1, target, sum - numbers[depth]); 
+        }
+    }
+}
