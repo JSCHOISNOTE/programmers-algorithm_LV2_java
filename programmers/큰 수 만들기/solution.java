7e8c77e92cd61103c0ee6b33dc30773215a29196
JEONG SEOK CHOI <109507834+JSCHOISNOTE@users.noreply.github.com>
2023-03-31 01:56:46 +0900

    uploaded at 2023. 3. 31. 오전 1:58:59

---
+++ programmers/큰 수 만들기/solution.java
@@ -0,0 +1,21 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42883
+
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+        
+        int index = 0;
+        int max = 0;
+        for(int i=0; i<number.length() - k; i++) {
+            max = 0;
+            for(int j = index; j<= k+i; j++) {
+                if(max < number.charAt(j)-'0') {
+                    max = number.charAt(j)-'0';
+                    index = j+1;
+                }
+            }
+            sb.append(max);
+        }
+        return sb.toString();
+    }
+}
