29261cb7d1be5298a30d8160eafc381635ed8df6
JEONG SEOK CHOI <109507834+JSCHOISNOTE@users.noreply.github.com>
2023-04-04 15:55:22 +0900

    uploaded at 2023. 4. 4. 오후 3:57:44

---
+++ programmers/2 x n 타일링/solution.java
@@ -0,0 +1,19 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12900
+
+import java.util.*;
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 2;
+        
+        for (int i = 2; i < n; i++) {
+            int num = arr[i - 1] + arr[i - 2];
+            arr[i] = num % 1000000007;
+        }
+        
+        return arr[n-1];
+    }
+}
